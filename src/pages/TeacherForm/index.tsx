e055592182a8c37731ce550f7596bac55dc736b0
macedo-developer <macedorenata.developer@gmail.com>
2020-08-05 21:25:02 -0300

    Using the Textarea component in the TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
+
 import warningIcon from "../../assets/icons/warning.svg";
 
 import "./styles.css";
@@ -21,6 +23,7 @@ function TeacherForm() {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
         <fieldset>
           <legend>Sobre a aula</legend>
