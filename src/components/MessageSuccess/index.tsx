083b6ac8866b3018645253e59a8a26138aa0b245
macedo-developer <macedorenata.developer@gmail.com>
2020-08-18 21:06:32 -0300

    New component MessageSuccess

--- src/components/MessageSuccess/index.tsx
+++ src/components/MessageSuccess/index.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useLocation, Link } from "react-router-dom";
+
+import feitoImg from "../../assets/feito.svg";
 
 import "./styles.css";
 
+interface MessageSuccessProps {
+  title: string;
+  description: string;
+}
+
 const MessageSuccess: React.FC = () => {
-  return <h1>Hello Success</h1>;
+  const location = useLocation();
+  const props = location.state as MessageSuccessProps;
+
+  return (
+    <div id="page-message-success">
+      <div className="background">
+        <div className="message">
+          <img src={feitoImg} alt="Feito" />
+          <h1>{props.title}</h1>
+          <p>{props.description}</p>
+
+          <Link to="/">Voltar para página Inicial</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MessageSuccess;
