e1db3ba1f698d7208b512f21740720ad79f47a87
macedo-developer <macedorenata.developer@gmail.com>
2020-08-20 22:27:20 -0300

    Styles page Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logoImg from "../../assets/logo.svg";
+import logoPrimaryImg from "../../assets/logo-primary.svg";
 import purpleHeartImg from "../../assets/icons/purple-heart.svg";
 
 import "./styles.css";
@@ -16,33 +17,43 @@ function Login() {
           <img src={logoImg} alt="Logo Proffy" />
           <p>Sua plataforma de estudos online.</p>
         </div>
-      </div>
-      <div className="login-container">
-        <h1>Fazer login</h1>
 
-        <form>
-          <Input label="Email" name="email" />
-          <Input label="Senha" name="password" />
+        <div className="login-container">
+          <img className="logo-mobile" src={logoPrimaryImg} alt="Logo Proffy" />
+          <form>
+            <h1>Fazer login</h1>
+            <Input label="Email" name="email" />
+            <Input label="Senha" name="password" />
 
-          <div className="form-footer">
-            <p>Lembrar-me</p>
-            <p>Esqueci minha senha</p>
+            <div className="form-footer">
+              <div className="check-group">
+                <input type="checkbox" name="remember" id="remember"></input>
+                <p>Lembrar-me</p>
+              </div>
+              <Link to="/">Esqueci minha senha</Link>
+            </div>
+
+            <button disabled type="submit">
+              Entrar
+            </button>
+          </form>
+
+          <div className="footer">
+            <div className="question">
+              <p>
+                Não tem conta?
+                <br />
+                <Link className="button" to="/">
+                  Cadastra-se
+                </Link>
+              </p>
+              <span>
+                É de graça
+                <img src={purpleHeartImg} alt="Coração" />
+              </span>
+            </div>
           </div>
-
-          <button type="submit">Entrar</button>
-        </form>
-      </div>
-
-      <div className="footer">
-        <div className="question">
-          <p>Não tem conta?</p>
-          <p>
-            É de graça
-            <img src={purpleHeartImg} alt="Coração" />
-          </p>
         </div>
-
-        <Link to="/">Cadastra-se</Link>
       </div>
     </div>
   );
