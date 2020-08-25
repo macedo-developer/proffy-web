bcae7093df5f94a03a3b98e9cbee141369e85be4
macedo-developer <macedorenata.developer@gmail.com>
2020-08-24 23:14:12 -0300

    Add routing request

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/TeacherForm";
@@ -11,6 +12,7 @@ function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" component={Login} exact />
+      <Route path="/register" component={Register} />
       <Route path="/landing" component={Landing} exact />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
