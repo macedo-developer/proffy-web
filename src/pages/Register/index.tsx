975a9d78eabd6d4426f06474e6ad5bcee9497021
macedo-developer <macedorenata.developer@gmail.com>
2020-08-24 23:14:37 -0300

    Create page Register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+function Register() {
+  return (
+    <div id="page-register">
+      <div className="page-register-content">
+        <h1>Register</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
