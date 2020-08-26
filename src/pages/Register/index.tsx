ff728d3389b591f059189439295881860b73e323
macedo-developer <macedorenata.developer@gmail.com>
2020-08-25 23:46:30 -0300

    Adjusting stucture HTML

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 
 import logoImg from "../../assets/logo.svg";
+import logoPrimaryImg from "../../assets/logo-primary.svg";
+import backImg from "../../assets/icons/back.svg";
 
 import "./styles.css";
 import Input from "../../components/Input";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
     <div id="page-register">
       <div className="page-register-content">
         <div className="register-container">
+          <header>
+            <Link to="/">
+              <img src={backImg} alt="Voltar" />
+            </Link>
+            <img src={logoPrimaryImg} alt="Logo Proffy" />
+          </header>
           <form>
             <h1>Cadastro</h1>
+
             <p>Preencha os dados abaixo para começar</p>
             <Input label="Nome" name="name" autoFocus />
             <Input label="Sobrenome" name="surname" />
