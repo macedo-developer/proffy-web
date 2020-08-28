91b5a0ef9b46d22aa8c9afb55003c89c36abbb90
macedo-developer <macedorenata.developer@gmail.com>
2020-08-27 22:53:01 -0300

    Structure HTML in Redefine password

--- src/pages/RedefinePassword/index.tsx
+++ src/pages/RedefinePassword/index.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/logo.svg";
+import backImg from "../../assets/icons/back.svg";
 
 import "./styles.css";
+import Input from "../../components/Input";
 
 function RedefinePassword() {
-  return <h1>Redefinir Senha</h1>;
+  return (
+    <div id="page-redefine">
+      <div className="page-redefine-content">
+        <div className="redefine-container">
+          <header>
+            <Link to="/">
+              <img src={backImg} alt="Votar para Login" />
+            </Link>
+          </header>
+
+          <form>
+            <h1>Eita, esqueceu sua senha?</h1>
+            <p>Não esquenta, vamos dar um jeito nisso.</p>
+
+            <Input label="Email" name="email" />
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+        <div className="register-logo-container">
+          <img src={logoImg} alt="Logo Proffy" />
+          <p>Sua plataforma de estudos online.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RedefinePassword;
