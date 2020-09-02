e95644f19d2f1aef18dfcc7d1f73c129965d5b3a
macedo-developer <macedorenata.developer@gmail.com>
2020-09-01 23:00:19 -0300

    Change to link PageHeader

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -16,7 +16,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
-        <Link to="/">
+        <Link to="/landing">
           <img src={backIcon} alt="Voltar" />
         </Link>
         <p>{props.page}</p>
