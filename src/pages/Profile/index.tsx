2e8ad8086ff7b5707a545c82fe1dbad9ac3699ce
macedo-developer <macedorenata.developer@gmail.com>
2020-09-09 22:30:29 -0300

    Creating Profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageHeader from "../../components/PageHeader";
+
+function Profile() {
+  return <PageHeader page="Meu perfil"></PageHeader>;
+}
+
+export default Profile;
