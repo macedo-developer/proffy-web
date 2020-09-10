cc4ea2b5fc8eac23c1c1720d44e62c9f07972ec3
macedo-developer <macedorenata.developer@gmail.com>
2020-09-09 22:31:39 -0300

    Creating routing profile

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import RedefinePassword from "./pages/RedefinePassword";
+import Profile from "./pages/Profile";
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/TeacherForm";
@@ -16,6 +17,7 @@ function Routes() {
       <Route path="/register" component={Register} />
       <Route path="/redefine-password" component={RedefinePassword} />
       <Route path="/landing" component={Landing} exact />
+      <Route path="/profile" component={Profile} exact />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
       <Route path="/success" component={MessageSuccess} />
