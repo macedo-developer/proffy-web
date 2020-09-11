c72a668d25510d2226456a9e50a4e074b697d196
macedo-developer <macedorenata.developer@gmail.com>
2020-09-10 21:21:35 -0300

    Structure basic profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+
 function Profile() {
-  return <PageHeader page="Meu perfil"></PageHeader>;
+  return (
+    <div id="page-profile">
+      <PageHeader page="Meu perfil"></PageHeader>
+    </div>
+  );
 }
 
 export default Profile;
