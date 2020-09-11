8fe3298b6a25a05e87076985891bf46705ee8c68
macedo-developer <macedorenata.developer@gmail.com>
2020-09-10 21:41:21 -0300

    Structure header Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -6,7 +6,16 @@ import "./styles.css";
 function Profile() {
   return (
     <div id="page-profile">
-      <PageHeader page="Meu perfil"></PageHeader>
+      <PageHeader page="Meu perfil">
+        <div className="infos-profile">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51935321?s=460&u=96e365fd38fcdabd5aa97ab462db5bc4499f49e2&v=4"
+            alt="Avatar"
+          />
+          <h2>Renata Macedo</h2>
+          <span>Matemática</span>
+        </div>
+      </PageHeader>
     </div>
   );
 }
