184f724d8962230edcb41cb425581368f1d45d21
macedo-developer <macedorenata.developer@gmail.com>
2020-11-23 23:36:26 -0300

    Remove console.log

--- src/utils/convertMinuteToHour.ts
+++ src/utils/convertMinuteToHour.ts
@@ -3,7 +3,5 @@ export default function convertMinuteToHour(time: number) {
   const hours = Math.floor(time / 60)
   const minutes = time % 60
 
-  console.log(`${hours}:${minutes}`);
-
   return `${hours}:${minutes}`;
 }
